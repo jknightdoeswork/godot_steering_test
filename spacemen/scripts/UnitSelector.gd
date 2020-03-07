eccf64d3784838ee6ebf827505ce0df5eecdfa42
Jason Knight <00jknight@gmail.com>
2020-03-06 20:54:00 -0600

    new

--- spacemen/scripts/UnitSelector.gd
+++ spacemen/scripts/UnitSelector.gd
@@ -1,15 +1,13 @@
 extends Node2D
-
 signal on_selection_changed
 
+onready var input_signals = $"../InputSignals"
 var selected_unit = null
-
-onready var input_signals = $InputSignals
 
 func _ready():
 	input_signals.connect("on_left_mouse_pressed", self, "on_left_mouse_button_pressed")
 	input_signals.connect("on_right_mouse_pressed", self, "on_right_mouse_button_pressed")
-	
+
 func on_left_mouse_button_pressed(position:Vector2):
 	if selected_unit != null:
 		print ("deselecting unit")
@@ -18,7 +16,7 @@ func on_left_mouse_button_pressed(position:Vector2):
 func on_right_mouse_button_pressed(position:Vector2):
 	if selected_unit != null:
 		print ("issuing arrive order")
-	
+
 func on_ship_clicked(ship):
 	print ("selecting unit " + str(ship))
 	selected_unit = ship
