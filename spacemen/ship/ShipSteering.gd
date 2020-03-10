738a7ad5836531275820bb7781e62c51ccd301b9
Jason Knight <00jknight@gmail.com>
2020-03-09 22:21:25 -0600

    some tweaks

--- spacemen/ship/ShipSteering.gd
+++ spacemen/ship/ShipSteering.gd
@@ -3,16 +3,17 @@ extends Node
 onready var identity		:= $"../Identity"
 onready var nearby			:= $"../NearbyEnemies"
 onready var kinematic_body 	:= $"../" as KinematicBody2D
-onready var agent 			:= GSAIKinematicBody2DAgent.new(kinematic_body, GSAIKinematicBody2DAgent.MovementType.COLLIDE)
+onready var agent 			:= GSAIKinematicBody2DAgent.new(kinematic_body)
 onready var arrive_target 	:= GSAIAgentLocation.new()
 onready var arrive 			:= GSAIArrive.new(agent, arrive_target)
 onready var look 			:= GSAILookWhereYouGo.new(agent)
 onready var blend 			:= GSAIBlend.new(agent)
-onready var avoid_prox		:= GSAIRadiusProximity.new(agent, [], 25.0)
+onready var avoid_prox		:= GSAIInfiniteProximity.new(agent, [])
 onready var avoid			:= GSAIAvoidCollisions.new(agent, avoid_prox)
 var _accel 					:= GSAITargetAcceleration.new()
 var _velocity 				:= Vector2.ZERO
 var _drag 					:= 0.1
+var enable_avoidance		:= true
 
 func _ready() -> void:
 	agent.position 					= GSAIUtils.to_vector3(kinematic_body.global_position)
@@ -25,37 +26,35 @@ func _ready() -> void:
 	agent.angular_drag_percentage 	= 0.4
 	agent.bounding_radius			= 10.0
 	arrive_target.position 			= agent.position
-	arrive.deceleration_radius 		= 100.0
-	arrive.arrival_tolerance 		= 50.0
+	arrive.deceleration_radius 		= 99.0
+	arrive.arrival_tolerance 		= 100.0
 	
 	look.alignment_tolerance = deg2rad(10.0)
 	look.deceleration_radius = deg2rad(30)
 	
-	blend.add(avoid, 1.0)
 	blend.add(arrive, 1.0)
 	blend.add(look, 1.0)
-
-
+	if enable_avoidance:
+		blend.add(avoid, 1.0)
 
 func issue_arrive_order(arrive_position:Vector2):
 	#print ("[ShipSteering] issue_arrive_order: %s" % [arrive_position])
 	arrive_target.position = GSAIUtils.to_vector3(arrive_position)
-	arrive_target.orientation = rand_range(0.0, 2.0*PI)
 	
 func _physics_process(delta: float) -> void:
-	
 	# Add nearby agents to avoidance
-	avoid_prox.agents.clear()
-	for u in nearby.nearby_units():
-		var other_steering = u.get_node("Steering")
-		if other_steering != null:
-			var other_agent = other_steering.agent
-			if other_agent != null:
-				avoid_prox.agents.append(other_agent)
+	if enable_avoidance:
+		avoid_prox.agents.clear()
+		for u in nearby.nearby_units():
+			var other_steering = u.get_node("Steering")
+			if other_steering != null:
+				var other_agent = other_steering.agent
+				if other_agent != null:
+					avoid_prox.agents.append(other_agent)
+				else:
+					printerr("[ShipSteering] node doesnt have agent")
 			else:
-				printerr("[ShipSteering] node doesnt have agent")
-		else:
-			printerr("[ShipSteering] node doesnt have steering")
+				printerr("[ShipSteering] node doesnt have steering")
 	blend.calculate_steering(_accel)
 	
 	agent._apply_steering(_accel, delta)
